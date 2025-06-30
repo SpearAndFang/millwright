d1aa944a4afb61216fb0de664210643b67394004
SpearAndFang <69551808+SpearAndFang@users.noreply.github.com>
2025-06-29 21:44:27 -0700

    v1.2.7

---
+++ ModSystem/SyncClientPacket.cs
@@ -0,0 +1,15 @@
+﻿using ProtoBuf;
+
+namespace millwright.ModSystem
+{
+    [ProtoContract(ImplicitFields = ImplicitFields.AllPublic)]
+    public class SyncClientPacket
+    {
+        public bool AllowSailDeconstruction;
+        public double BrakeResistanceModifier;
+        public double SailCenteredModifier;
+        public double SailAngledModifier;
+        public double SailWideModifier;
+        public double SailRotationModifier;
+    }
+}
--- ModSystem/millwrightSystem.cs
+++ ModSystem/millwrightSystem.cs
@@ -1,10 +1,21 @@
 namespace Millwright.ModSystem
 {
-    using Vintagestory.API.Common;
+    using millwright.ModSystem;
     using Millwright.ModConfig;
+    using ProtoBuf;
+    using System.Collections.Generic;
+    using Vintagestory.API.Client;
+    using Vintagestory.API.Common;
+    using Vintagestory.API.MathTools;
+    using Vintagestory.API.Server;
+    using Vintagestory.API.Util;
 
     public class MillwrightSystem : ModSystem
     {
+
+        private IServerNetworkChannel serverChannel;
+        private ICoreAPI api;
+
         public void RegisterClasses(ICoreAPI api)
         {
             api.RegisterBlockClass("BlockWindmillRotorEnhanced", typeof(BlockWindmillRotorEnhanced));
@@ -32,6 +43,7 @@ namespace Millwright.ModSystem
 
         public override void Start(ICoreAPI api)
         {
+            this.api = api;
             base.Start(api);
             api.World.Logger.Event("started 'Millwright' mod");
             this.RegisterClasses(api);
@@ -54,6 +66,70 @@ namespace Millwright.ModSystem
             api.World.Config.SetBool("AllowSailDeconstruction", ModConfig.Loaded.AllowSailDeconstruction);
 
             base.StartPre(api);
+        }
+
+
+        public override void StartClientSide(ICoreClientAPI capi)
+        {
+            capi.Network.RegisterChannel("millwright")
+                .RegisterMessageType<SyncClientPacket>()
+                .SetMessageHandler<SyncClientPacket>(packet =>
+                {
+
+                    ModConfig.Loaded.AllowSailDeconstruction = packet.AllowSailDeconstruction;
+                    this.Mod.Logger.Event($"Received AllowSailDeconstruction of {packet.AllowSailDeconstruction} from server");
+
+                    ModConfig.Loaded.BrakeResistanceModifier = packet.BrakeResistanceModifier;
+                    this.Mod.Logger.Event($"Received BrakeResistanceModifier of {packet.BrakeResistanceModifier} from server");
+
+                    ModConfig.Loaded.SailCenteredModifier = packet.SailCenteredModifier;
+                    this.Mod.Logger.Event($"Received SailCenteredModifier of {packet.SailCenteredModifier} from server");
+
+                    ModConfig.Loaded.SailAngledModifier = packet.SailAngledModifier;
+                    this.Mod.Logger.Event($"Received SailAngledModifier of {packet.SailAngledModifier} from server");
+
+                    ModConfig.Loaded.SailWideModifier = packet.SailWideModifier;
+                    this.Mod.Logger.Event($"Received SailWideModifier of {packet.SailWideModifier} from server");
+
+                    ModConfig.Loaded.SailRotationModifier = packet.SailRotationModifier;
+                    this.Mod.Logger.Event($"Received SailRotationModifier of {packet.SailRotationModifier} from server");
+
+                });
+        }
+
+
+        public override void StartServerSide(ICoreServerAPI sapi)
+        {
+            // send connecting players the config settings
+            sapi.Event.PlayerJoin += this.OnPlayerJoin; // add method so we can remove it in dispose to prevent memory leaks
+            // register network channel to send data to clients
+            this.serverChannel = sapi.Network.RegisterChannel("millwright")
+                .RegisterMessageType<SyncClientPacket>()
+                .SetMessageHandler<SyncClientPacket>((player, packet) => { /* do nothing. idk why this handler is even needed, but it is */ });
+        }
+
+
+        private void OnPlayerJoin(IServerPlayer player)
+        {
+            // send the connecting player the settings it needs to be synced
+            this.serverChannel.SendPacket(new SyncClientPacket
+            {
+                AllowSailDeconstruction = ModConfig.Loaded.AllowSailDeconstruction,
+                BrakeResistanceModifier = ModConfig.Loaded.BrakeResistanceModifier,
+                SailCenteredModifier = ModConfig.Loaded.SailCenteredModifier,
+                SailAngledModifier = ModConfig.Loaded.SailAngledModifier,
+                SailWideModifier = ModConfig.Loaded.SailWideModifier,
+                SailRotationModifier = ModConfig.Loaded.SailRotationModifier
+            }, player);
+        }
+
+        public override void Dispose()
+        {
+            // remove our player join listener so we dont create memory leaks
+            if (this.api is ICoreServerAPI sapi)
+            {
+                sapi.Event.PlayerJoin -= this.OnPlayerJoin;
+            }
         }
     }
 }
